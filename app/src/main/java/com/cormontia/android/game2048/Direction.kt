e4d8f450054437f823516928c52fc4e9ed8e66d0
FractionalRadix <nerostjust@gmail.com>
2022-03-31 21:59:36 +0200

    Add background color to tiles (for numbers 2, 4, 8, and 16).

---
+++ app/src/main/java/com/cormontia/android/game2048/Direction.kt
@@ -0,0 +1,5 @@
+package com.cormontia.android.game2048
+
+enum class Direction {
+    UP, RIGHT, DOWN, LEFT
+}
--- app/src/main/java/com/cormontia/android/game2048/GameBoardView.kt
+++ app/src/main/java/com/cormontia/android/game2048/GameBoardView.kt
@@ -1,7 +1,9 @@
 package com.cormontia.android.game2048
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Canvas
+import android.graphics.Color.rgb
 import android.graphics.Paint
 import android.graphics.Rect
 import android.util.AttributeSet
@@ -19,7 +21,11 @@ class GameBoardView : View {
 
     private var gameBoard = GameState(HashMap())
 
+    // Paint for the text
     private val blackPaint = Paint()
+
+    // Paints for the different background colors:
+    val backgroundPaints : MutableMap<Int, Paint> = mutableMapOf()
 
     constructor(context: Context) : super(context) {
         init(context, null, 0)
@@ -41,11 +47,43 @@ class GameBoardView : View {
         blackPaint.style = Paint.Style.STROKE
         blackPaint.strokeWidth = 4f
         blackPaint.textSize = 40f
+
+        run {
+            val backgroundPaint = Paint()
+            backgroundPaint.color = rgb(255, 255, 0)
+            backgroundPaints[2] = backgroundPaint
+        }
+
+        run {
+            val backgroundPaint = Paint()
+            backgroundPaint.color = rgb(255, 192, 0)
+            backgroundPaints[4] = backgroundPaint
+        }
+
+        run {
+            val backgroundPaint = Paint()
+            backgroundPaint.color = rgb(255, 128, 0)
+            backgroundPaints[8] = backgroundPaint
+        }
+
+        run {
+            val backgroundPaint = Paint()
+            backgroundPaint.color = rgb(255, 64, 0)
+            backgroundPaints[16] = backgroundPaint
+        }
+
     }
 
     fun updateGameState(gameState: GameState) {
         this.gameBoard = gameState
         invalidate()    // Force repaint.
+    }
+
+    //TODO!- Just testing if I can send an Intent to the MainActivity...
+    private fun sendUpIntent() {
+        val intent = Intent(context, MainActivity::class.java)
+        intent.putExtra("direction", Direction.UP)
+        context.sendBroadcast(intent)
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -84,13 +122,20 @@ class GameBoardView : View {
                 val x = paddingLeft + x0 + 100 * (c-1)
                 val y = paddingTop + y0 + 100 * (r-1)
                 val smallRect = Rect(x, y, x+90, y+90)
-                canvas.drawRect(smallRect, blackPaint)
 
                 val fieldValue = gameBoard.state[Coor(r,c)]
                 if (fieldValue != null) {
+
+                    val backgroundPaint = backgroundPaints[fieldValue]
+                    if (backgroundPaint != null) {
+                        canvas.drawRect(smallRect, backgroundPaint)
+                    }
+
                     //TODO!~ Adjust these offsets...
                     canvas.drawText(fieldValue.toString(), (x + 50).toFloat(), (y + 50).toFloat(), blackPaint)
                 }
+
+                canvas.drawRect(smallRect, blackPaint)
             }
         }
     }
